cc452a667d5e2aafae3a1637760a2c256d13411c
Artem Trifonov <artemis.levitt@gmail.com>
2025-03-02 14:14:00 +0100

    Implement fetching from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
-// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import {useEffect, useState} from "react";
 
 function App() {
+    const [amount, setAmount] = useState(1);
+    const [fromCurrency, setFromCurrency] = useState('EUR');
+    const [toCurrency, setToCurrency] = useState('USD');
+
+    useEffect(function () {
+        async function convert() {
+            const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`)
+        const data = await res.json();
+        console.log(data);
+        }
+
+        convert();
+    }, [])
+
     return (
         <div>
-            <input type="text" />
-            <select>
+            <input
+                type="text"
+                value={amount}
+                onChange={event => setAmount(Number(event.target.value))}
+            />
+            <select
+                value={fromCurrency}
+                onChange={event => setFromCurrency(event.target.value)}>
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="CAD">CAD</option>
-                <option value="INR">INR</option>
+                <option value="ILS">ILS</option>
             </select>
-            <select>
+            <select
+                value={toCurrency}
+                onChange={event => setToCurrency(event.target.value)}
+            >
                 <option value="USD">USD</option>
                 <option value="EUR">EUR</option>
                 <option value="CAD">CAD</option>
-                <option value="INR">INR</option>
+                <option value="ILS">ILS</option>
             </select>
             <p>OUTPUT</p>
         </div>
